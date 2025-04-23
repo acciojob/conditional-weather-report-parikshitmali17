c1264b4befa3a96aae2cc0838f5151aefdd9563f
Parikshit <Parikshitmali17@gmail.com>
2025-04-23 13:51:10 +0530

    space

--- src/components/WheatherDisplay.js
+++ src/components/WheatherDisplay.js
@@ -6,7 +6,7 @@ function WheatherDisplay(props) {
   return (
     <div>
         
-          <p>Temperature:<span style={{color: props.data.temperature >20 ? "red" :" blue"}}>{props.data.temperature}</span></p>
+          <p>Temperature: <span style={{color: props.data.temperature >20 ? "red" :" blue"}}>{props.data.temperature}</span></p>
         
         <p>Conditions: {props.data.conditions}</p>
     </div>
