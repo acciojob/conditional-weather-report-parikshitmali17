636a91286ec784e0ff067140ca5b8fe4068a897e
Parikshit <Parikshitmali17@gmail.com>
2025-04-23 13:23:08 +0530

    d

--- src/components/WheatherDisplay.js
+++ src/components/WheatherDisplay.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function WheatherDisplay(props) {
     console.log(props)
+    
   return (
     <div>
         
-          <p><span style={{color: props.data.temperature >20 ? "red" :" blue"}}>Temperature:{props.data.temperature}</span></p>
+       { useEffect((e)=>{
+ <p><span style={{color: props.data.temperature >20 ? "red" :" blue"}}>Temperature:{props.data.temperature}</span></p>
+},[props.data.temperature])}
+         
         
         <p>Conditions: {props.data.conditions}</p>
     </div>
